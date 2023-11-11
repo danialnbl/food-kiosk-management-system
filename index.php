b7a4dd2884c7a98b5e0248ca57c1c671b247b862
danialnbl <danialnabil0208@gmail.com>
2023-11-11 18:02:33 +0800

    Team section and about us descriptions

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 
     <!-- Luar  -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
     <link href="assets/css/style.scss" rel="stylesheet">
@@ -26,7 +29,8 @@
             <nav id="navbar" class="navbar">
                 <ul>
                     <li><a class="nav-link scrollto active" href="#">Home</a></li>
-                    <li><a class="nav-link scrollto" href="#">About Us</a></li>
+                    <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+                    <li><a class="nav-link scrollto" href="#team">Our Team</a></li>
                     <li><a class="nav-link scrollto" href="#">Login</a></li>
                     <li><a class="getstarted scrollto" href="#">Get Started</a></li>
                 </ul>
@@ -64,13 +68,14 @@
             <div class="row content">
                 <div class="col-lg-6">
                     <p>
-                        Was born to make sure you never lose a parcel again.
-                        Be it tracking your orders internationally, locally or in your building we are here to help.
+                        Welcome to Fakulti Komputeran's innovative Food Kiosk Management System, a cutting-edge solution designed to revolutionize 
+                        the way we experience food services on campus. Developed by a team of dedicated professionals at Fakulti Komputeran, 
+                        our system aims to enhance efficiency, convenience, and overall satisfaction for both customers and vendors within the university community.
                     </p>
                 </div>
                 <div class="col-lg-6 pt-4 pt-lg-0">
                     <p>
-                        Log parcels for all major carriers including Pos Malaysia, J&T Express, Shopee Express, DHL Ecommerce and many more
+                        At Fakulti Komputeran, we envision a seamless and enjoyable food experience on campus. Our Food Kiosk Management System is geared towards creating a dynamic environment that fosters quick, efficient, and delightful interactions for all users.
                     </p>
                     <a href="#" class="btn-learn-more">Learn More</a>
                 </div>
@@ -78,6 +83,91 @@
         </div>
     </section>
     <!-- End About Us Section -->
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Team</h2>
+          <p>At the heart of our innovative Food Kiosk Management System is a team of passionate and skilled individuals from Fakulti Komputeran.
+            Together, we bring a diverse range of expertise, creativity, and dedication to the table, ensuring the success and continuous 
+            improvement of our cutting-edge solution.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-1.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Walter White</h4>
+                <span>Chief Executive Officer</span>
+                <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Sarah Jhonson</h4>
+                <span>Product Manager</span>
+                <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="300">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>William Anderson</h4>
+                <span>CTO</span>
+                <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-4.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Amanda Jepson</h4>
+                <span>Accountant</span>
+                <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
 </body>
 
 </html>
