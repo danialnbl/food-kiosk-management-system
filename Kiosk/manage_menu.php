b267a4a507809c6afa6f26782160284e736a3783
danialnbl <danialnabil0208@gmail.com>
2024-01-02 00:26:00 +0800

    update image, add

--- Kiosk/manage_menu.php
+++ Kiosk/manage_menu.php
@@ -43,6 +43,7 @@ if (!isset($_SESSION['User'])) {
                         <th>Description</th>
                         <th>Price</th>
                         <th>Status</th>
+                        <th>Image</th>
                         <th>Actions</th>
                       </tr>
                     </thead>
@@ -56,12 +57,14 @@ if (!isset($_SESSION['User'])) {
                       while ($row = mysqli_fetch_array($ret)) {
                         $i++;
                       ?>
-                        <tr>
+                        <tr id="<?php echo $row['MenuID'] ?>">
                           <th scope="row"><?php echo $i; ?></th>
                           <td><?php echo $row['ItemName']; ?></td>
                           <td><?php echo $row['ItemDesc']; ?></td>
                           <td>RM <?php echo $row['ItemPrice']; ?></td>
                           <td><?php echo $row['Availability']; ?></td>
+                          <td hidden><?php echo $row['Stock']; ?></td>
+                          <td><img style="height: 100px; width: 100px;" src="data:image;base64,  <?php echo $row['ItemImage']  ?> " alt="Test"></td>
                           <td>
                             <form method="post">
                               <div class="dropdown">
@@ -69,11 +72,11 @@ if (!isset($_SESSION['User'])) {
                                   <i class="bx bx-dots-vertical-rounded"></i>
                                 </button>
                                 <div class="dropdown-menu">
-                                  <a class="dropdown-item opn" data-bs-toggle="modal" data-bs-target="#edit-meal" href="javascript:void(0);" data-id="">
+                                  <a class="dropdown-item opn" data-bs-toggle="modal" data-bs-target="#edit-menu" href="javascript:void(0);" data-id="<?php echo $row['MenuID']; ?>">
                                     <i class="bx bx-edit-alt me-1"></i>
                                     Edit
                                   </a>
-                                  <a class="dropdown-item del" data-id=""><i class="bx bx-trash me-1"></i> Delete</a>
+                                  <a class="dropdown-item del" data-id="<?php echo $row['MenuID']; ?>"><i class="bx bx-trash me-1"></i> Delete</a>
                                 </div>
                               </div>
                             </form>
@@ -84,6 +87,7 @@ if (!isset($_SESSION['User'])) {
                   </table>
                 </div>
               </div>
+
             </div>
             <!-- / Content -->
           </div>
@@ -92,7 +96,7 @@ if (!isset($_SESSION['User'])) {
     </div>
 
     <!-- Add Menu Modal -->
-    <form method="post">
+    <form method="post" enctype="multipart/form-data">
       <div class="modal fade" id="add-meal" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-sm" role="document">
           <div class="modal-content">
@@ -104,39 +108,37 @@ if (!isset($_SESSION['User'])) {
               <div class="row">
                 <div class="col mb-3">
                   <label for="menuName" class="form-label">Menu Name</label>
-                  <input type="text" id="menuName" class="form-control" placeholder="Enter Name" value="" />
+                  <input type="text" id="menuName" name="menuName" class="form-control" placeholder="Enter Name" value="" />
                 </div>
                 <div class="col mb-3">
-                  <label for="menuPrice" class="form-label">Availability</label>
-                  <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example" name="mealCal">
+                  <label for="menuAvailability" class="form-label">Availability</label>
+                  <select class="form-select" id="menuAvailability" aria-label="Default select example" name="menuAvailability">
                     <option selected>Open this select menu</option>
                     <option id="menuAvailable" value="Available">Available</option>
-                    <option id="menuNotAvailable" value="NotAvailable">Not Available</option>
+                    <option id="menuNotAvailable" value="Not Available">Not Available</option>
                   </select>
                 </div>
               </div>
               <div class="row">
-              </div>
-              <div class="row">
                 <div class="col mb-3">
                   <label for="menuPrice" class="form-label">Price</label>
-                  <input type="text" id="menuPrice" class="form-control" placeholder="Enter Price" value="" />
+                  <input type="text" id="menuPrice" name="menuPrice" class="form-control" placeholder="Enter Price" value="" />
                 </div>
                 <div class="col mb-3">
                   <label for="menuStock" class="form-label">Stock</label>
-                  <input type="text" id="menuStock" class="form-control" placeholder="Enter Stock" value="" />
+                  <input type="text" id="menuStock" name="menuStock" class="form-control" placeholder="Enter Stock" value="" />
                 </div>
               </div>
               <div class="row">
-              <div class="col mb-3">
+                <div class="col mb-3">
                   <label for="menuDesc" class="form-label">Menu Description</label>
-                  <textarea type="text" id="menuDesc" class="form-control" placeholder="Enter Description" value="" rows="3"></textarea>
-              </div>
+                  <textarea type="text" id="menuDesc" name="menuDesc" class="form-control" placeholder="Enter Description" value="" rows="3"></textarea>
+                </div>
               </div>
               <div class="row">
                 <div class="col mb-3">
-                  <label for="formFile" class="form-label">Menu Image</label>
-                  <input class="form-control" type="file" id="formFile" onchange="preview()">
+                  <label for="formFile" class="form-label">Menu Image (Cannot be edit after this)</label>
+                  <input class="form-control" type="file" id="formFile" name="formFile" onchange="preview()">
                 </div>
               </div>
               <div class="row">
@@ -158,7 +160,7 @@ if (!isset($_SESSION['User'])) {
 
     <!-- Edit Menu Modal -->
     <form method="post">
-      <div class="modal fade" id="edit-meal" tabindex="-1" aria-hidden="true">
+      <div class="modal fade" id="edit-menu" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-sm" role="document">
           <div class="modal-content">
             <div class="modal-header">
@@ -168,12 +170,12 @@ if (!isset($_SESSION['User'])) {
             <div class="modal-body">
               <div class="row">
                 <div class="col mb-3">
-                  <label for="menuName" class="form-label">Menu Name</label>
-                  <input type="text" id="menuName" class="form-control" placeholder="Enter Name" value="" />
+                  <label for="menuNameEdit" class="form-label">Menu Name</label>
+                  <input type="text" id="menuNameEdit" class="form-control" placeholder="Enter Name" value="" />
                 </div>
                 <div class="col mb-3">
-                  <label for="menuPrice" class="form-label">Availability</label>
-                  <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example" name="mealCal">
+                  <label for="menuAvailability" class="form-label">Availability</label>
+                  <select class="form-select" id="menuAvailabilityEdit" aria-label="Default select example" name="menuAvailability">
                     <option selected>Open this select menu</option>
                     <option id="menuAvailable" value="Available">Available</option>
                     <option id="menuNotAvailable" value="NotAvailable">Not Available</option>
@@ -181,47 +183,126 @@ if (!isset($_SESSION['User'])) {
                 </div>
               </div>
               <div class="row">
+                <div class="col mb-3">
+                  <label for="menuPriceEdit" class="form-label">Price</label>
+                  <input type="text" id="menuPriceEdit" class="form-control" placeholder="Enter Price" value="" />
+                </div>
+                <div class="col mb-3">
+                  <label for="menuStockEdit" class="form-label">Stock</label>
+                  <input type="text" id="menuStockEdit" class="form-control" placeholder="Enter Stock" value="" />
+                </div>
               </div>
               <div class="row">
                 <div class="col mb-3">
-                  <label for="menuPrice" class="form-label">Price</label>
-                  <input type="text" id="menuPrice" class="form-control" placeholder="Enter Price" value="" />
-                </div>
-                <div class="col mb-3">
-                  <label for="menuStock" class="form-label">Stock</label>
-                  <input type="text" id="menuStock" class="form-control" placeholder="Enter Stock" value="" />
+                  <label for="menuDescEdit" class="form-label">Menu Description</label>
+                  <textarea type="text" id="menuDescEdit" class="form-control" placeholder="Enter Description" value="" rows="3"></textarea>
                 </div>
               </div>
               <div class="row">
-              <div class="col mb-3">
-                  <label for="menuDesc" class="form-label">Menu Description</label>
-                  <textarea type="text" id="menuDesc" class="form-control" placeholder="Enter Description" value="" rows="3"></textarea>
+                <div class="col mb-3">
+                  <label for="formFile" class="form-label">Menu Image</label>
+                  <input class="form-control" type="file" id="formFileEdit" onchange="preview()">
                 </div>
               </div>
-                
+              <div class="row">
+                <div class="col mb-3">
+                  <img id="frameEdit" src="" class="img-fluid" style="height: 200px;" />
+                </div>
               </div>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
                 Close
               </button>
-              <button type="submit" name="addBtn" class="btn btn-primary">Save changes</button>
+              <input type="hidden" name="id" id="id">
+              <button type="submit" name="editBtn" class="btn btn-primary">Save changes</button>
             </div>
           </div>
         </div>
       </div>
+      </div>
     </form>
+    <!-- / Layout wrapper -->
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
     <script>
-            function preview() {
-                frame.src = URL.createObjectURL(event.target.files[0]);
-            }
-            function clearImage() {
-                document.getElementById('formFile').value = null;
-                frame.src = "";
-            }
-    </script>                   
+      var id;
+      $(".opn").click(function() {
+        id = $(this).data('id');
+        var col = $("#".concat(id, " > td"));
+        var unitVal = $("#menuAvailabilityEdit option").filter(function() {
+          return $(this).html() == col[3].innerText;
+        }).val()
+        console.log(col[0].innerText);
+        $("#id").val(id);
+        $("#menuNameEdit").val(col[0].innerText);
+        $("#menuDescEdit").val(col[1].innerText);
+        $("#menuPriceEdit").val(col[2].innerText);
+        $("#menuAvailabilityEdit").val(unitVal).change();
+        $("#menuStockEdit").val(col[4].innerText);
+        $("#frameEdit").val(col[4].innerText);
+      });
+
+      function preview() {
+        frame.src = URL.createObjectURL(event.target.files[0]);
+      }
+
+      function clearImage() {
+        document.getElementById('formFile').value = null;
+        frame.src = "";
+      }
+    </script>
     <script src="../assets/js/dashboards-analytics.js"></script>
   </body>
 
   </html>
+
+
+
+  <!-- CRUD Function -->
+  <?php
+  if (isset($_POST['addBtn'])) {
+
+    $menuName = $_POST['menuName'];
+    $menuAvailability = $_POST['menuAvailability'];
+    $menuPrice = $_POST['menuPrice'];
+    $menuStock = $_POST['menuStock'];
+    $menuDesc = $_POST['menuDesc'];
+
+    //declare variables
+    $image = $_FILES['formFile']['tmp_name'];
+    $name = $_FILES['formFile']['name'];
+    $image = base64_encode(file_get_contents(addslashes($image)));
+
+    $query = mysqli_query($conn, "INSERT INTO menu (KioskID, ItemName, ItemDesc, ItemPrice, Availability, Stock, ItemImage) VALUES (1,'$menuName', '$menuDesc','$menuPrice','$menuAvailability','$menuStock','$image')");
+
+    if ($query) {
+      echo '
+           <script type="text/javascript">
+                window.location.href="manage_menu.php";
+            </script>
+            ';
+    } else {
+      echo '
+      <script type="text/javascript">
+                window.location.href="manage_menu.php";
+            </script>
+           ';
+    }
+
+    // $image = $_FILES['formFile']['tmp_name'];
+    // $name = $_FILES['formFile']['name'];
+    // $image = file_get_contents(addslashes($image));
+
+    // $query = mysqli_query($conn, "INSERT INTO image (ImageName, ImageBlob, MenuID) VALUES ('$name', '$image',1)");
+
+
+  }
+
+  //Retrieve image from database and display it on html webpage
+  function displayImageFromDatabase()
+  {
+    //use global keyword to declare conn inside a function
+    global $conn;
+  }
+  ?>
 <?php } ?>
