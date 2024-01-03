75027dbe076a0a89eb212be12662adecb1fa6d69
danialnbl <danialnabil0208@gmail.com>
2024-01-03 18:25:34 +0800

    update user icon

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
       <li class="nav-item navbar-dropdown dropdown-user dropdown">
         <a class="nav-link dropdown-toggle hide-arrow d-flex align-items-center" href="javascript:void(0);" data-bs-toggle="dropdown">
           <div class="avatar ">
-            <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+            <img src="../assets/img/avatars/default_user.png" alt class="w-px-40 h-auto rounded-circle" />
           </div>
         </a>
         <ul class="dropdown-menu dropdown-menu-end">
