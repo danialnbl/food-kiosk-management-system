70a3d10ea8cd559e43722ef06a3e2e22c99e8eb6
danialnbl <danialnabil0208@gmail.com>
2024-01-06 17:29:26 +0800

    migrate db

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,12 @@
 <!-- connect.php -->
 <!-- To connect between php scripting and database. -->
 <?php
-define("DATABASE_HOST", "localhost");
-define("DATABASE_USER", "root");
+define("DATABASE_HOST", "10.26.30.17");
+define("DATABASE_USER", "cb22151");
+define("DATABASE_PASSWORD", "cb22151");
 
 // To establish a connection to database and save in Sconn
-$conn = mysqli_connect(DATABASE_HOST, DATABASE_USER);
+$conn = mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD);
 
 // If connection failed then dsplay mysql error
 if (mysqli_connect_errno())
@@ -14,7 +15,7 @@ if (mysqli_connect_errno())
 	}
 
 // To select one particular database to be used
-mysqli_select_db($conn,"miniproject") or die( "Could not open products database");
+mysqli_select_db($conn,"cb22151") or die( "Could not open products database");
 
 //set the default time zone to use in Malaysia
 date_default_timezone_set('Asia/Kuala_Lumpur');
