0d46e094349fd23127c1619841c286f659bac771
danialnbl <danialnabil0208@gmail.com>
2024-01-16 02:27:40 +0800

    update qr vendor, customer

--- registerstudent.php
+++ registerstudent.php
@@ -2,39 +2,44 @@
 <html>
 
 <head>
-    <title>Register</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- connect css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style2.scss">
-    <!-- Favicon -->
-    <link rel="icon" href="https://umpsa.edu.my/themes/pana/favicon.ico" />
-    <?php include('./includes/headsettings.php'); ?>
-        <script src="./assets/vendor/libs/jquery/jquery.js"></script>
-    <?php
+  <title>Register</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- connect css -->
+  <link rel="stylesheet" type="text/css" href="assets/css/style2.scss">
+  <!-- Favicon -->
+  <link rel="icon" href="https://umpsa.edu.my/themes/pana/favicon.ico" />
+  <?php include('./includes/headsettings.php'); ?>
+  <script src="./assets/vendor/libs/jquery/jquery.js"></script>
+  <?php
 
-    include './includes/connect.php';
-    // QR Library
-    require_once './assets/vendor/phpqrcode/qrlib.php';
-    
+  include './includes/connect.php';
+  // QR Library
+  require_once './assets/vendor/phpqrcode/qrlib.php';
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST['username'];
-        // $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        $password = $_POST['password'];
-        $email = $_POST['email'];
-        $fullName = $_POST['fullName'];
-        $phoneNum = $_POST['phoneNum'];
 
-        //QR
-        $pathQr = './assets/img/qr/';
-        $qrCode = $pathQr . time() . ".png";
-        QRcode::png($phoneNum . "uid=" . $username, $qrCode, 'H', 4, 4);
-        $qrImage = base64_encode(file_get_contents(addslashes($qrCode)));
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    // $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $fullName = $_POST['fullName'];
+    $phoneNum = $_POST['phoneNum'];
 
-        $query = mysqli_query($conn, "INSERT INTO user (UserName, Password, FullName, Email, NumPhone, UserType, UserQR) VALUES ('$username','$password', '$fullName','$email','$phoneNum','Customer','$qrImage')");
+    $query = mysqli_query($conn, "INSERT INTO user (UserName, Password, FullName, Email, NumPhone, UserType) VALUES ('$username','$password', '$fullName','$email','$phoneNum','Customer')");
 
-        if ($query) {
-            echo '
+    if ($query) {
+
+      $userid = mysqli_insert_id($conn);
+
+      //QR
+      $pathQr = './assets/img/qr/';
+      $qrCode = $pathQr.$userid.".png";
+      QRcode::png($userid, $qrCode, 'H', 4, 4);
+      $qrImage = base64_encode(file_get_contents(addslashes($qrCode)));
+
+      $queryQR = mysqli_query($conn, "UPDATE user SET UserQR = '$qrImage' WHERE UserID = '$userid'");
+
+      echo '
 <script type="text/javascript">
 $(document).ready(function(){
 Swal.fire({
@@ -49,8 +54,8 @@ Swal.fire({
 
 </script>
     ';
-        } else {
-            echo '
+    } else {
+      echo '
 <script type="text/javascript">
       $(document).ready(function(){
         Swal.fire({
@@ -65,31 +70,31 @@ Swal.fire({
       });
     </script>
    ';
-        }
     }
-    ?>
-    <!-- HTML form for user registration -->
+  }
+  ?>
+  <!-- HTML form for user registration -->
 </head>
 
 <body>
-    <a href="index.php">
-        <img src="assets/img/logo.png" alt="Vendor" width="180" height="100">
-    </a>
-    <div class="form">
-        <div class="container">
-            <h1 class="title"><i>Customer Registration Form</i></h1>
-            <form action="" method="post" name="registration">
-                Email: <input type="text" name="email" placeholder="Email" required />
-                Username: <input type="text" name="username" placeholder="Username" required />
-                Password: <input type="password" name="password" placeholder="Password" required />
-                Full Name: <input type="text" name="fullName" placeholder="Full Name" required />
-                Phone Number: <input type="text" name="phoneNum" placeholder="Phone Number" required />
-                <input name="submit" type="submit" value="Submit" />
-            </form>
-            <a href="registration.php"><button>Back</button></a>
-        </div>
+  <a href="index.php">
+    <img src="assets/img/logo.png" alt="Vendor" width="180" height="100">
+  </a>
+  <div class="form">
+    <div class="container">
+      <h1 class="title"><i>Customer Registration Form</i></h1>
+      <form action="" method="post" name="registration">
+        Email: <input type="text" name="email" placeholder="Email" required />
+        Username: <input type="text" name="username" placeholder="Username" required />
+        Password: <input type="password" name="password" placeholder="Password" required />
+        Full Name: <input type="text" name="fullName" placeholder="Full Name" required />
+        Phone Number: <input type="text" name="phoneNum" placeholder="Phone Number" required />
+        <input name="submit" type="submit" value="Submit" />
+      </form>
+      <a href="registration.php"><button>Back</button></a>
     </div>
-    </form>
+  </div>
+  </form>
 </body>
 
 </html>
--- vendor.php
+++ vendor.php
@@ -2,57 +2,64 @@
 <html>
 
 <head>
-    <title>Register</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- connect css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style2.scss">
-    <script src="./assets/vendor/libs/jquery/jquery.js"></script>
-    <?php
-    include './includes/connect.php';
-    include('./includes/headsettings.php');
-    require_once './assets/vendor/phpqrcode/qrlib.php';
-    ?>
-    <!-- HTML form for user registration -->
+  <title>Register</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- connect css -->
+  <link rel="stylesheet" type="text/css" href="assets/css/style2.scss">
+  <script src="./assets/vendor/libs/jquery/jquery.js"></script>
+  <?php
+  include './includes/connect.php';
+  include('./includes/headsettings.php');
+  require_once './assets/vendor/phpqrcode/qrlib.php';
+  ?>
+  <!-- HTML form for user registration -->
 </head>
 
 <body>
-    <a href="index.php">
-        <img src="assets/img/logo.png" alt="Vendor" width="180" height="100">
-    </a>
-    <div class="form">
-        <h1 class="title"><i>Vendor Registration Form</i></h1>
-        <div class="container">
-            <form method="post" name="registration">
-                Vendor Name: <input type="text" name="vendorName" placeholder="Business Name" required />
-                Email: <input type="text" name="email" placeholder="Email" required />
-                Password: <input type="password" name="password" placeholder="Password" required />
-                Phone Number: <input type="text" name="phoneNum" placeholder="Phone Number" required />
-                <input id="submitBtn" name="submitBtn" type="submit" value="Submit" />
-            </form>
-            <a href="registration.php"><button>Back</button></a>
-            <p>This form will be send to the administration for approval.</p>
-        </div>
+  <a href="index.php">
+    <img src="assets/img/logo.png" alt="Vendor" width="180" height="100">
+  </a>
+  <div class="form">
+    <h1 class="title"><i>Vendor Registration Form</i></h1>
+    <div class="container">
+      <form method="post" name="registration">
+        Vendor Name: <input type="text" name="vendorName" placeholder="Business Name" required />
+        Email: <input type="text" name="email" placeholder="Email" required />
+        Password: <input type="password" name="password" placeholder="Password" required />
+        Phone Number: <input type="text" name="phoneNum" placeholder="Phone Number" required />
+        <input id="submitBtn" name="submitBtn" type="submit" value="Submit" />
+      </form>
+      <a href="registration.php"><button>Back</button></a>
+      <p>This form will be send to the administration for approval.</p>
     </div>
+  </div>
 
-    <?php
+  <?php
 
-    if (isset($_POST['submitBtn'])) {
+  if (isset($_POST['submitBtn'])) {
 
-        $vendorName = $_POST['vendorName'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $phoneNum = $_POST['phoneNum'];
+    $vendorName = $_POST['vendorName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $phoneNum = $_POST['phoneNum'];
 
-        //QR
-        $pathQr = './assets/img/qr/';
-        $qrCode = $pathQr . time() . ".png";
-        QRcode::png($phoneNum . "uid=" . $vendorName, $qrCode, 'H', 4, 4);
-        $qrImage = base64_encode(file_get_contents(addslashes($qrCode)));
 
-        $query = mysqli_query($conn, "INSERT INTO vendor (VendorName, VendorEmail, VendorPassword, VendorNum, ApprovalStatus,KioskID,VendorQR) VALUES ('$vendorName','$email','$password','$phoneNum','Pending',1, '$qrImage')");
 
-        if ($query) {
-            echo '
+    $query = mysqli_query($conn, "INSERT INTO vendor (VendorName, VendorEmail, VendorPassword, VendorNum, ApprovalStatus,KioskID) VALUES ('$vendorName','$email','$password','$phoneNum','Pending',1)");
+
+    if ($query) {
+
+      $vendorID = mysqli_insert_id($conn);
+
+      //QR
+      $pathQr = './assets/img/qr/';
+      $qrCode = $pathQr.$vendorID. ".png";
+      QRcode::png($vendorID, $qrCode, 'H', 4, 4);
+      $qrImage = base64_encode(file_get_contents(addslashes($qrCode)));
+
+      $queryQR = mysqli_query($conn, "UPDATE vendor SET VendorQR = '$qrImage' WHERE VendorID = '$vendorID'");
+
+      echo '
 <script type="text/javascript">
 $(document).ready(function(){
 Swal.fire({
@@ -67,8 +74,8 @@ Swal.fire({
 
 </script>
     ';
-        } else {
-            echo '
+    } else {
+      echo '
 <script type="text/javascript">
       $(document).ready(function(){
         Swal.fire({
@@ -83,10 +90,10 @@ Swal.fire({
       });
     </script>
    ';
-        }
     }
+  }
 
-    ?>
+  ?>
 
 </body>
 
